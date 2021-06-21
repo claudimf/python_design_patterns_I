f87a8f401dd51f57f678da20a6ea14be94a748b6
Claudi Martins de Freitas <claudi.freitas.prs@synergiaconsultoria.com.br>
2021-06-21 17:49:14 -0300

    Makes course class "Duck Typing"

--- src/impostos_strategy/calculador_de_impostos.py
+++ src/impostos_strategy/calculador_de_impostos.py
@@ -1,20 +1,15 @@
 class Calculador_de_impostos(object):
 
-  def realiza_calculo(self, orcamento, imposto):
-
-    if 'ICMS' == imposto:
-      icms_calculado = orcamento.valor * 0.1
-      print(icms_calculado)
-
-    elif 'ISS' == imposto:
-      iss_calculado = orcamento.valor * 0.06
-      print(iss_calculado)
+    def realiza_calculo(self, orcamento, imposto):
+        valor = imposto.calcula(orcamento)
+        print(valor)
 
 if __name__ == '__main__':
 
     from orcamento import Orcamento
+    from impostos import ISS, ICMS
 
     orcamento = Orcamento(500.0)
     calculador_de_impostos = Calculador_de_impostos()
-    calculador_de_impostos.realiza_calculo(orcamento, 'ICMS') # imprimirá 50
-    calculador_de_impostos.realiza_calculo(orcamento, 'ISS') # imprimirá 30
+    calculador_de_impostos.realiza_calculo(orcamento, ISS())
+    calculador_de_impostos.realiza_calculo(orcamento, ICMS())
